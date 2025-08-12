b4bdf2b63312d740499d416c928bb486eee581d9
antonio86itna <48077495+antonio86itna@users.noreply.github.com>
2025-08-12 18:48:50 +0200

    Add search results page and AJAX filters

--- assets/js/frontend-vehicle-list.js
+++ assets/js/frontend-vehicle-list.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($) {
     // Vehicle type filter
-    $('#vehicle-type-filter').on('change', function() {
+    $(document).on('change', '#vehicle-type-filter', function() {
         const selectedType = $(this).val();
         const $cards = $('.crcm-vehicle-card');
-        
+
         if (selectedType === '') {
             $cards.show();
         } else {
@@ -16,32 +16,69 @@ jQuery(document).ready(function($) {
                 }
             });
         }
-        
+
         updateResultsCount();
     });
-    
+
+    // Location filter
+    $(document).on('change', '#location-filter', function() {
+        const selectedLocation = $(this).val();
+        const $cards = $('.crcm-vehicle-card');
+
+        if (selectedLocation === '') {
+            $cards.show();
+        } else {
+            $cards.each(function() {
+                const cardLocation = $(this).data('location');
+                if (cardLocation === selectedLocation) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+
+        updateResultsCount();
+    });
+
     // Price sorting
-    $('#price-sort-filter').on('change', function() {
+    $(document).on('change', '#price-sort-filter', function() {
         const sortType = $(this).val();
         const $grid = $('#vehicles-grid');
         const $cards = $('.crcm-vehicle-card').toArray();
-        
+
         if (sortType === 'price-asc' || sortType === 'price-desc') {
             $cards.sort(function(a, b) {
                 const priceA = parseFloat($(a).data('daily-rate'));
                 const priceB = parseFloat($(b).data('daily-rate'));
-                
+
                 if (sortType === 'price-asc') {
                     return priceA - priceB;
                 } else {
                     return priceB - priceA;
                 }
             });
-            
+
             $grid.empty().append($cards);
         }
     });
-    
+
+    // AJAX pagination
+    $(document).on('click', '.crcm-pagination a', function(e) {
+        e.preventDefault();
+        const url = $(this).attr('href');
+
+        $.get(url, function(response) {
+            const $html = $('<div>').html(response);
+            const $content = $html.find('.crcm-vehicle-list-container').html();
+            $('.crcm-vehicle-list-container').html($content);
+
+            $('#vehicle-type-filter').trigger('change');
+            $('#location-filter').trigger('change');
+            updateResultsCount();
+        });
+    });
+
     // Book now button click
     $(document).on('click', '.crcm-book-now', function() {
         const vehicleId = $(this).data('vehicle-id');
@@ -49,7 +86,7 @@ jQuery(document).ready(function($) {
         const returnDate = $(this).data('return-date');
         const pickupTime = $(this).data('pickup-time');
         const returnTime = $(this).data('return-time');
-        
+
         // Build booking URL with parameters
         const bookingUrl = new URL(window.location.origin + '/booking-form/');
         bookingUrl.searchParams.set('vehicle', vehicleId);
@@ -57,10 +94,10 @@ jQuery(document).ready(function($) {
         bookingUrl.searchParams.set('return_date', returnDate);
         bookingUrl.searchParams.set('pickup_time', pickupTime);
         bookingUrl.searchParams.set('return_time', returnTime);
-        
+
         window.location.href = bookingUrl.toString();
     });
-    
+
     // Update results count
     function updateResultsCount() {
         const visibleCards = $('.crcm-vehicle-card:visible').length;
